e96ced1d5e4dbe8a4d89ff939d533c7117424fbb
Herman Lymanskyi <herman.lymanskyi@acronis.com>
2024-02-28 18:42:42 +0200

    get all files from directory including subfolders

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <filesystem>
+#include <vector>
 
-int main()
+namespace fs = std::filesystem;
+
+std::vector<fs::path> getFilesInDirectory(const fs::path& directoryPath) {
+  std::vector<fs::path> filesList;
+  for (const auto& entry : fs::recursive_directory_iterator(directoryPath)) {
+    if (fs::is_regular_file(entry)) {
+      filesList.push_back(entry.path());
+    }
+  }
+
+  return filesList;
+}
+
+int main(int argc, const char** argv)
 {
-  std::cout << "Hello, World!" << std::endl;
+  if (argc != 2)
+  {
+    std::cout << "Wrong nuber of arguments.\n";
+    std::cout << "Usage wargaming_test_task <path to dir>\n";
+    return -1;
+  }
+
+  const fs::path directoryPath = argv[1];
+
+  std::vector<fs::path> files = getFilesInDirectory(directoryPath);
+
+  std::cout << "Files in directory and subdirectories:" << std::endl;
+
+  for (const auto& file : files) {
+    std::cout << file << std::endl;
+  }
+
   return 0;
 }
